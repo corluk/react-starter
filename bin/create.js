ce6940e53044ca1f57e8d46c02a229e43958cdde
Osman Corluk <corluk@hotmail.com>
2021-05-10 01:56:55 +0300

    d

--- bin/create.js
+++ bin/create.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
 const packageJSON  = require("../package.json") 
+const exec = require("child_process").execSync
+const dependencies =  Object.keys(packageJSON.dependencies) 
+const devDependencies = Object.keys(packageJSON.devDependencies)
+ 
+console.log(process.argv)
+exec(` yarn add ${dependencies.join(" ")}`)
+exec(` yarn add -D  ${devDependencies.join(" ")}`)
 
-console.log(packageJSON)
